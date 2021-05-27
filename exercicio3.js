edb2e59977db6e3a39ebb43e96f66ef8e0f1dcdc
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-27 11:53:52 -0300

    Adicionando exercicio3.js

---
+++ exercicio3.js
@@ -0,0 +1,29 @@
+
+function divisao(num1,num2){
+
+    
+
+        return num1/num2 
+        
+        }
+
+      
+
+    
+
+
+
+
+
+
+console.log("O resultado da divisão é  : " +divisao(14,7))
+
+
+if (divisao(14,7) % 2 === 0){
+
+    console.log("Ele é par")
+}
+
+
+
+
