ce8a15299ebbf4fde5e8b5016c9be6f2974f844e
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-27 11:56:35 -0300

    Adicionando exercicio4.js

---
+++ exercicio4.js
@@ -0,0 +1,20 @@
+for (i = 1; i <= 100 ; i++){
+
+if (i === 50){
+
+    break;
+}
+
+
+console.log(i)
+}
+
+for (i = 1; i<=100 ; i++){
+
+    if(i === 50){
+
+        continue
+    }
+   console.log(i)
+}
+
